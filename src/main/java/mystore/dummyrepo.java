374794059a1e6877d2c5fbcb695c178e4707bec3
arunitech03 <arunitech03@gmail.com>
2019-03-06 16:46:35 +0530

    cleaning unused files

--- src/main/java/mystore/dummyrepo.java
+++
@@ -1,9 +0,0 @@
-package mystore;
-
-import org.springframework.data.repository.CrudRepository;
-import mystore.product.entity.dummyentity;
-
-public interface dummyrepo extends CrudRepository<dummyentity, Integer> {
-
-	}
-
